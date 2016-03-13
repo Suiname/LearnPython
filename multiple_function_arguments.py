955194bb3d251173bbdcef126dbc6f1831424d50
Jason Tham <jtham@sbcglobal.net>
2016-03-13 00:42:10 -0600

    completed mutiple function arguments lesson

---
+++ multiple_function_arguments.py
@@ -0,0 +1,36 @@
+def foo(first, second, third, *therest):
+    print "First: %s" % first
+    print "Second: %s" % second
+    print "Third: %s" % third
+    print "And all the rest... %s" % list(therest)
+
+foo("one","two","three","four", "five", "six")
+
+def bar(first, second, third, **options):
+    if options.get("action") == "sum":
+        print "The sum is: %d" % (first + second + third)
+
+    if options.get("number") == "first":
+        return first
+
+result = bar(1, 2, 3, action = "sum", number = "first")
+print "Result: %d" % result
+
+# Fill in the foo and bar functions so they can receive a variable amount of arguments (3 or more) The foo function must return the amount of extra arguments received. The bar must return True if the argument with the keyword magicnumber is worth 7, and False otherwise.
+# edit the functions prototype and implementation
+def foo(a, b, c, *args):
+    return len(args)
+
+def bar(a, b, c, **args):
+    return args.get("magicnumber") == 7
+
+
+# test code
+if foo(1,2,3,4) == 1:
+    print "Good."
+if foo(1,2,3,4,5) == 2:
+    print "Better."
+if bar(1,2,3,magicnumber = 6) == False:
+    print "Great."
+if bar(1,2,3,magicnumber = 7) == True:
+    print "Awesome!"
